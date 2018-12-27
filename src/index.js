d53af133c7139fffb9402c46f682e8ca90e9fb27
OpportunityLiu <opportunity@live.in>
2018-12-27 13:23:33 +0800

    add htmlmin ignore

--- src/index.js
+++ src/index.js
@@ -1,41 +1,52 @@
 import mermaid from 'mermaid'
 
-function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
+function escapeHtml(unsafe)
+{
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
-const mermaidChart = (code) => {
-  try {
+const mermaidChart = (code) =>
+{
+  try
+  {
     mermaid.parse(code)
-    return `<div class="mermaid">${escapeHtml(code)}</div>`
-  } catch ({ str, hash }) {
+    return `<!-- htmlmin:ignore --><div class="mermaid">${escapeHtml(code)}</div><!-- htmlmin:ignore -->`
+  } catch ({ str, hash })
+  {
     return `<pre class="mermaid-error">${escapeHtml(str)}</pre>`
   }
 }
 
-const MermaidPlugin = (md) => {
+const MermaidPlugin = (md) =>
+{
   md.mermaid = mermaid
-  mermaid.loadPreferences = (preferenceStore) => {
+  mermaid.loadPreferences = (preferenceStore) =>
+  {
     let mermaidTheme = preferenceStore.get('mermaid-theme')
-    if (mermaidTheme === undefined) {
+    if (mermaidTheme === undefined)
+    {
       mermaidTheme = 'default'
     }
     let ganttAxisFormat = preferenceStore.get('gantt-axis-format')
-    if (ganttAxisFormat === undefined) {
+    if (ganttAxisFormat === undefined)
+    {
       ganttAxisFormat = '%Y-%m-%d'
     }
     mermaid.initialize({
       theme: mermaidTheme,
-      gantt: { axisFormatter: [
-        [ganttAxisFormat, (d) => {
-          return d.getDay() === 1
-        }]
-      ]}
+      gantt: {
+        axisFormatter: [
+          [ganttAxisFormat, (d) =>
+          {
+            return d.getDay() === 1
+          }]
+        ]
+      }
     })
     return {
       'mermaid-theme': mermaidTheme,
@@ -44,14 +55,17 @@ const MermaidPlugin = (md) => {
   }
 
   const temp = md.renderer.rules.fence.bind(md.renderer.rules)
-  md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+  md.renderer.rules.fence = (tokens, idx, options, env, slf) =>
+  {
     const token = tokens[idx]
     const code = token.content.trim()
-    if (token.info === 'mermaid') {
+    if (token.info === 'mermaid')
+    {
       return mermaidChart(code)
     }
     const firstLine = code.split(/\n/)[0].trim()
-    if (firstLine === 'gantt' || firstLine === 'sequenceDiagram' || firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) {
+    if (firstLine === 'gantt' || firstLine === 'sequenceDiagram' || firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/))
+    {
       return mermaidChart(code)
     }
     return temp(tokens, idx, options, env, slf)
